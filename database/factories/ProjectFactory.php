02d7f07c74fb53405d6928a6f1d2677e9417de09
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-08 12:37:59 -0300

    feat: create user and project models

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => collect(fake()->words(5))->join(' '),
+            'description' => fake()->randomHtml(),
+            'ends_at' => fake()->dateTimeBetween('now', '+3 days'),
+            'status' => fake()->randomElement(['open', 'closed']),
+            'tech-stack' => fake()->randomElements(['php', 'laravel', 'javascript', 'vue', 'react', 'angular', 'next.js', 'tailwind', 'python'], random_int(1, 5)),
+            'created_by' => User::factory()
+        ];
+    }
+}
